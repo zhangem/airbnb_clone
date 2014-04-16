f0873dbd4ec76d0c84c8975c61b21a2174a8dbe2
Ashley Rae Lee <student@epicodus.com>
2014-04-16 12:09:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lairbnb::Application.config.secret_key_base = '2464fc3dcf5ac562be43e036f6fda3095769812ec1290eb830b7d7445bf87f279e9d931316141097db011b5c150b3cef5b971d37c71f9e79665d5bd01de5699b'
