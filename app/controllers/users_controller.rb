d6ae796cf3fded29496249beb33bd9e68fe31327
Ashley Rae Lee <student@epicodus.com>
2014-04-16 13:43:08 -0700

    creates devise user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create(user_params)
+    @user = User.new
+    @user.save
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
